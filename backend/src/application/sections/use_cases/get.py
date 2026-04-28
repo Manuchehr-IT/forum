91b8a7da69d1f5f20565f5a242f25f60a14f01bf
ManuchehrTG <toshmatovmanuchehr@gmail.com>
2026-04-29 02:28:36 +0500

    fix section get [application]

--- backend/src/application/sections/use_cases/get.py
+++ backend/src/application/sections/use_cases/get.py
@@ -2,6 +2,7 @@ from uuid import UUID
 
 from src.application.decorators import handle_domain_errors
 from src.application.sections.dtos import SectionDTO
+from src.application.sections.mappers import SectionMapper
 from src.application.sections.queries import GetSectionQuery, GetSectionQueryType
 from src.domain.sections.entities import Section
 from src.domain.sections.repository import SectionRepository
@@ -24,22 +25,10 @@ class GetSection:
 		else:
 			raise ValueError(f"Unknown query type: {query.query_type}")
 
-		return self._to_dto(section)
+		return SectionMapper.to_dto(section)
 
 	async def _get_by_id(self, section_id: UUID) -> Section:
 		return await self.section_repo.get_by_id(section_id)
 
 	async def _get_by_code(self, section_code: str) -> Section:
 		return await self.section_repo.get_by_code(section_code)
-
-	def _to_dto(self, section: Section) -> SectionDTO:
-		return SectionDTO(
-			id=section.id,
-			code=section.code,
-			openai_prompt=section.openai_prompt,
-			tech_version=section.tech_version,
-			enable_openai=section.enable_openai,
-			allow_hide=section.allow_hide,
-			created_at=section.created_at,
-			updated_at=section.updated_at,
-		)
